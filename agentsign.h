4f3df14b508614bdaaa286575624ba28f89b20b8
Dmitriy Denisov <dmitriy.denisov@osinit.com>
2021-09-06 18:11:35 +0300

    add agentsign

---
+++ agentsign.h
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//Файл: agentsign.h
+//Автор: DDN
+//Data: 07.31.2002
+//Описание: сигнатуры для агентов
+// OC LINUX, WINDOWS 9x/NT 
+/////////////////////////////////////////////////////////////////////////////////////////
+#ifndef __AGENTSIGN_H_2001_05_11_12_00
+#define __AGENTSIGN_H_2001_05_11_12_00
+
+#define	AGENT_SERIAL_NUM "AgentSerialNumbe" 	// Серийный номер агента
+
+#define AGENT_FILE_HASH "AgentFileMD5Hash" // Свертка на файлы
+
+#define	AGENT_IDENTIF	"The MSDN Library is the essential reference for developers, with"	\
+					"more than a gigabyte of technical programming information, inclu"	\
+					"ding sample code, documentation, technical articles, the Microso"	\
+					"ft Developer Knowledge Base, and anything else you might need.  "	
+          
+#define AGENT_IP_ADDR "AgentIPAdresAdre" //IP адрес
+
+#define AGENT_PORT_ADDR "AgentPortAdressA"  // Порт
+#define AGENT_PORT_ADDR1 "AgentPortAdress1"  // Порт
+#define AGENT_PORT_ADDR2 "AgentPortAdress2"  // Порт
+
+#endif
